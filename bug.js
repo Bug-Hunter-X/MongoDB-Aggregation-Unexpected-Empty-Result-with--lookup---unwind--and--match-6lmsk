1c03671c1303a8eaa44f6e692c72ef1f7eb53407
BugHunter-X <askzenai@gmail.com>
2024-12-22 03:50:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+const pipeline = [
+  {
+    $lookup: {
+      from: "collectionB",
+      localField: "_id",
+      foreignField: "foreignKey",
+      as: "results",
+    },
+  },
+  {
+    $unwind: "$results",
+  },
+  {
+    $match: {
+      "results.someField": {
+        $exists: true,
+      },
+    },
+  },
+];
+
+await collectionA.aggregate(pipeline).toArray();
+```
